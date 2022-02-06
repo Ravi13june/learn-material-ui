b78a3afe6665e7a9b42b94a45c8244629dbdc8e7
Ravi <gehalotkishan@gmail.com>
2022-02-06 17:16:31 +0530

    material-ui set up

---
+++ src/components/LoginSection.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Container from "@mui/material/Container";
+import { Grid, Typography } from "@mui/material";
+const LoginSection = () => {
+  return (
+    <Container maxWidth="xl">
+      <Grid container>
+        <Grid item>
+          <Typography>Hi, I am Ravi Kishan dsds fsd sfsaf sdf</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>ssdfsf</Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default LoginSection;
